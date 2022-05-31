7cb67df851e2bc765cec19c539efc12c729e433b
Lawrence (Yuxin) Fan <48743988+yx-fan@users.noreply.github.com>
2022-05-31 14:50:03 +0800

    Data Clean for DB

--- DataCleanforDB/main.py
+++ DataCleanforDB/main.py
@@ -19,7 +19,27 @@ dataset.columns
 #        u'Score-4', u'Score-3', u'Score-2', u'Score-1'],
 #       dtype='object')
 
+# Deal with Genres many to many relations
 data1 = dataset[['MAL_ID', 'Genres']]
-
 # Generate Genres and Anime many to many dataset
 data_genres = data1.set_index(['MAL_ID']).apply(lambda x: x.str.split(', ').explode()).reset_index()
+os.chdir("/Users/lawrence/Documents/GitHub/Web_Development/CIS550Project/DataCleanforDB")
+data_genres.to_csv('Anime_Genres.csv')
+
+# Deal with Producers many to many relations
+data2 = dataset[['MAL_ID', 'Producers']]
+data_producers = data2.set_index(['MAL_ID']).apply(lambda x: x.str.split(', ').explode()).reset_index()
+os.chdir("/Users/lawrence/Documents/GitHub/Web_Development/CIS550Project/DataCleanforDB")
+data_producers.to_csv('Anime_Producers.csv')
+
+# Deal with Licensors many to many relations
+data3 = dataset[['MAL_ID', 'Licensors']]
+data_licensors = data3.set_index(['MAL_ID']).apply(lambda x: x.str.split(', ').explode()).reset_index()
+os.chdir("/Users/lawrence/Documents/GitHub/Web_Development/CIS550Project/DataCleanforDB")
+data_licensors.to_csv('Anime_Licensors.csv')
+
+# Deal with Studios many to many relations
+data4 = dataset[['MAL_ID', 'Studios']]
+data_studios = data4.set_index(['MAL_ID']).apply(lambda x: x.str.split(', ').explode()).reset_index()
+os.chdir("/Users/lawrence/Documents/GitHub/Web_Development/CIS550Project/DataCleanforDB")
+data_studios.to_csv('Anime_Studios.csv')
