8a4d9db0876ebb83755948a3933dd149878f639d
Meng Li <dameng@seas.upenn.edu>
2022-07-09 11:22:11 -0400

    added server and client folder

---
+++ project/server/routes.js
@@ -0,0 +1,82 @@
+const config = require('./config.json')
+const mysql = require('mysql');
+const e = require('express');
+
+// TODO: fill in your connection details here
+const connection = mysql.createConnection({
+    host: config.rds_host,
+    user: config.rds_user,
+    password: config.rds_password,
+    port: config.rds_port,
+    database: config.rds_db
+});
+connection.connect();
+
+// ********************************************
+//             USER-SPECIFIC ROUTES
+// ********************************************
+async function user_profile(req, res) {
+    const userid = req.query.id
+    connection.query(`SELECT Name, Birthday
+    FROM user
+    WHERE user_id = ${userid}`, function (error, results, fields) {
+
+        if (error) {
+            res.json({ error: error })
+        } else if (results) {
+            res.json({ results: results })
+        }
+    })
+}
+
+async function user_watched(req, res) {
+    const userid = req.query.id
+    connection.query(`SELECT Name
+    FROM animelist
+    JOIN anime ON animelist.anime_id = anime.Anime_ID
+    WHERE user_id = ${userid} AND watching_status = 2`, function (error, results, fields) {
+
+        if (error) {
+            res.json({ error: error })
+        } else if (results) {
+            res.json({ results: results })
+        }
+    })
+}
+
+async function user_watching(req, res) {
+    const userid = req.query.id
+    connection.query(`SELECT Name
+    FROM animelist
+    JOIN anime ON animelist.anime_id = anime.Anime_ID
+    WHERE user_id = ${userid} AND watching_status = 1`, function (error, results, fields) {
+
+        if (error) {
+            res.json({ error: error })
+        } else if (results) {
+            res.json({ results: results })
+        }
+    })
+}
+
+async function user_rated(req, res) {
+    const userid = req.query.id
+    connection.query(`SELECT Name
+    FROM animelist
+    JOIN anime ON animelist.anime_id = anime.Anime_ID
+    WHERE user_id = ${userid} AND score <> 0`, function (error, results, fields) {
+
+        if (error) {
+            res.json({ error: error })
+        } else if (results) {
+            res.json({ results: results })
+        }
+    })
+}
+
+module.exports = {
+    user_profile,
+    user_watched,
+    user_watching,
+    user_rated
+}
---
+++ project/server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mysql = require('mysql');
+var cors = require('cors')
+
+
+const routes = require('./routes')
+const config = require('./config.json')
+
+const app = express();
+
+// whitelist localhost 3000
+app.use(cors({ credentials: true, origin: ['http://localhost:3000'] }));
+
+
+app.get('/user/watched', routes.user_watched)
+app.get('/user/watching', routes.user_watching)
+app.get('/user/rated', routes.user_rated)
+app.get('/user/profile', routes.user_profile)
+
+
+app.listen(config.server_port, () => {
+    console.log(`Server running at http://${config.server_host}:${config.server_port}/`);
+});
+
+module.exports = app;
