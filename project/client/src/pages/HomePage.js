78353764214e5db5f3433645fbd04d4dacf9654e
allencc1989 <82770483+allencc1989@users.noreply.github.com>
2022-07-26 11:37:41 -0400

    Search bar update

--- project/client/src/pages/HomePage.js
+++ project/client/src/pages/HomePage.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import {
-  Table,
-  Pagination,
-  Select
-} from 'antd'
-
 import MenuBar from '../components/MenuBar';
-import SearchBoxTitle from '../components/SearchBoxTitle'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Form, Button} from 'react-bootstrap';
-import {getTopAnime, getTopManga} from '../fetcher';
+import {Form, Button, Container} from 'react-bootstrap';
+import {getTitle, getTopAnime, getTopManga} from '../fetcher';
 import AnimeList from '../components/AnimeList';
-const { Option } = Select;
+
 
 
 class HomePage extends React.Component {
@@ -21,11 +14,14 @@ class HomePage extends React.Component {
 
     this.state = {
          topAnimes:[],
-         topMangas:[]
+         topMangas:[],
+         searchAnimes:null
     }
 
+    this.titleInput = React.createRef();
+
   }
-  topAnimesReq = async() =>{
+  /*topAnimesReq = async() =>{
 
     const url = "http://127.0.0.1:8080/main/topAnime"
     const res = await fetch(url);
@@ -41,25 +37,33 @@ class HomePage extends React.Component {
     const resJson = await res.json()
     console.log(resJson)
     this.setState({topMangas: resJson})
-  };
+  };*/
  
 
   componentDidMount() {
 
    getTopAnime().then(res =>{
-      this.setState({topAnimes: res})
+      this.setState({topAnimes: res.results})
     })
 
    getTopManga().then(res => {
-      this.setState({topMangas: res})
+      this.setState({topMangas: res.results})
     })
      
     //this.topAnimesReq();
     //this.topMangasReq();
-
+    
 
   }
 
+  getTitleSearch =(e)=>{
+
+    e.preventDefault();
+    getTitle().then(res =>{
+      this.setState({searchAnimes: res.results})
+    })
+
+  }
 
   render() {
 
@@ -68,22 +72,50 @@ class HomePage extends React.Component {
         <div>
           <MenuBar/>
         </div> 
-        <SearchBoxTitle/>
-
+        
+        <div className='mx-auto p-4' style={{width:"800px"}}>
+        <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Title Search"
+              className="me-2"
+              aria-label="Title Search"
+              ref = {this.titleInput}
+            />
+            <Button variant="outline-success" type="submit" onClick={this.searchAnimes}>Search</Button>
+          </Form>
+        </div>
+      
+      <Container>
+        {this.searchAnimes?
         <div className='container-fluid movie-app'>
+          <p>Title Search result</p>
+          <div className='row'>
+              <AnimeList animes={this.state.searchAnimes}/>
+          </div>
+        </div>:
+        <></>
+        }
+      </Container>
+      
+      <Container>
+        <div className='container-fluid movie-app'>
+          <p>Top Animes</p>
           <div className='row'>
               <AnimeList animes={this.state.topAnimes}/>
           </div>
         </div>
-
+      </Container>
+      <Container>
         <div className='container-fluid movie-app'>
+          <p>Top Mangas</p>
           <div className='row'>
               <AnimeList animes={this.state.topMangas}/>
           </div>
         </div>
-
+      </Container>
       </div>
-
+       
     )
 
   }
