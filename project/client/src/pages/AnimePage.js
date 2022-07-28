d0c9f66c30bcac4847bfca83fe029c6b57e7b64b
Lawrence (Yuxin) Fan <48743988+yx-fan@users.noreply.github.com>
2022-07-29 01:24:02 +0800

    update anime pages

--- project/client/src/pages/AnimePage.js
+++ project/client/src/pages/AnimePage.js
@@ -82,13 +82,14 @@ class AnimePage extends React.Component {
 
 
           <Divider />
-          <Divider />
+         
 
 
           {this.state.userAlsoWatchDetails ? <div style={{ width: '70vw', margin: '0 auto', marginTop: '2vh' }}>
 
           {/* <Card> */}
             <h3>People Also Watch</h3>
+            <Divider />
           {/* <CardBody> */}
 
             <Row gutter='30' justify='center'>
@@ -157,67 +158,74 @@ class AnimePage extends React.Component {
 
 
           <Divider />
-          <Divider />
 
 
           {this.state.TopScoreAnimeInSameGenresDetails ? <div style={{ width: '70vw', margin: '0 auto', marginTop: '2vh' }}>
 
-            <Card>
               <h3>Top Score Anime in Same Genres</h3>
-            <CardBody>
+              <Divider />
+
   
-              <Row gutter='30' align='middle' justify='center'>
-  
-                <Col flex={2} style={{ textAlign: 'center' }}>
-                <h5> {this.state.TopScoreAnimeInSameGenresDetails[0].Name} </h5>  
+              <Row gutter='30' justify='center'>
+
+
+                <Col span={4} style={{ textAlign: 'center' }}>
+                  <Card style={{height: '25vh', width: '19vh'}}>
                   {!this.state.TopScoreAnimeInSameGenresDetails[0] ? (
                     <h5> No results found </h5>
                   ) : (
                     <img src={this.state.TopScoreAnimeInSameGenresDetails[0].url} referrerpolicy="no-referrer" alt={"Alsowatch1"} style={{height:'25vh'}}/>
                   )}
+                  </Card>
+                  {this.state.TopScoreAnimeInSameGenresDetails[0].Name}
                 </Col>
   
-                <Col flex={2} style={{ textAlign: 'center' }}>
-                <h5> {this.state.TopScoreAnimeInSameGenresDetails[1].Name} </h5>  
+                <Col span={4} style={{ textAlign: 'center' }}>
+                  <Card style={{height: '25vh', width: '19vh'}}>
                   {!this.state.TopScoreAnimeInSameGenresDetails[1] ? (
                     <h5> No results found </h5>
                   ) : (
                     <img src={this.state.TopScoreAnimeInSameGenresDetails[1].url} referrerpolicy="no-referrer" alt={"Alsowatch2"} style={{height:'25vh'}}/>
                   )}
+                  </Card>
+                  {this.state.TopScoreAnimeInSameGenresDetails[1].Name}
                 </Col>
   
-                <Col flex={2} style={{ textAlign: 'center' }}>
-                <h5> {this.state.TopScoreAnimeInSameGenresDetails[2].Name} </h5>  
+                <Col span={4} style={{ textAlign: 'center' }}>
+                  <Card style={{height: '25vh', width: '19vh'}}>
                   {!this.state.TopScoreAnimeInSameGenresDetails[2] ? (
                     <h5> No results found </h5>
                   ) : (
                     <img src={this.state.TopScoreAnimeInSameGenresDetails[2].url} referrerpolicy="no-referrer" alt={"Alsowatch3"} style={{height:'25vh'}}/>
                   )}
+                  </Card>
+                  {this.state.TopScoreAnimeInSameGenresDetails[2].Name}
                 </Col>
   
   
-                <Col flex={2} style={{ textAlign: 'center' }}>
-                <h5> {this.state.TopScoreAnimeInSameGenresDetails[3].Name} </h5>  
+                <Col span={4} style={{ textAlign: 'center' }}>
+                  <Card style={{height: '25vh', width: '19vh'}}>  
                   {!this.state.TopScoreAnimeInSameGenresDetails[3] ? (
                     <h5> No results found </h5>
                   ) : (
                     <img src={this.state.TopScoreAnimeInSameGenresDetails[3].url} referrerpolicy="no-referrer" alt={"Alsowatch4"} style={{height:'25vh'}}/>
                   )}
+                  </Card>
+                  {this.state.TopScoreAnimeInSameGenresDetails[3].Name}
                 </Col>
   
-                <Col flex={2} style={{ textAlign: 'center' }}>
-                <h5> {this.state.TopScoreAnimeInSameGenresDetails[4].Name} </h5>  
+                <Col span={4} style={{ textAlign: 'center' }}>
+                  <Card style={{height: '25vh', width: '19vh'}}>
                   {!this.state.TopScoreAnimeInSameGenresDetails[4] ? (
                     <h5> No results found </h5>
                   ) : (
                     <img src={this.state.TopScoreAnimeInSameGenresDetails[4].url} referrerpolicy="no-referrer" alt={"Alsowatch5"} style={{height:'25vh'}}/>
                   )}
+                  </Card>
+                  {this.state.TopScoreAnimeInSameGenresDetails[4].Name}  
                 </Col>
               </Row>
-  
-  
-            </CardBody>
-            </Card>
+
   
             </div> : null}
 
