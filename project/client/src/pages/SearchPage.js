e53d81f5945edb3261a115af48c3e58a2da64e9e
xuchenlia <57568408+xuchenlia@users.noreply.github.com>
2022-07-29 17:21:34 +0800

    update search page
    update search page

--- project/client/src/pages/SearchPage.js
+++ project/client/src/pages/SearchPage.js
@@ -1,23 +1,73 @@
-// Import necessary packages
-
 import React from 'react';
+import MenuBar from '../components/MenuBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Form, Button} from 'react-bootstrap';
+import {getTitle, getTopAnime, getTopManga} from '../fetcher';
+import AnimeList from '../components/AnimeList';
 
 
-
-
-
-
-// Page layout
 
 class SearchPage extends React.Component {
 
+  constructor(props) {
+    super(props)
 
-    
+    this.state = {
+         topAnimes:[],
+         topMangas:[],
+         searchAnimes:null
     }
-    
-    export default SearchPage
 
+    this.titleInput = React.createRef();
 
+  }
+  
+  componentDidMount() {
 
+   getTopAnime().then(res =>{
+      this.setState({topAnimes: res.results})
+    })
 
+   getTopManga().then(res => {
+      this.setState({topMangas: res.results})
+    })
+     
+  }
 
+  getTitleSearch =(e)=>{
+
+    e.preventDefault();
+
+    var titleInput = this.titleInput.current.value;
+    getTitle(1,titleInput).then(res =>{
+      this.setState({searchAnimes: res.results})
+    })
+
+  }
+
+  render() {
+
+    return (
+        <div>
+            <div>
+            <MenuBar/>
+            </div> 
+            
+            <div className='mx-auto p-4' style={{width:"800px"}}>
+            <Form className="d-flex">
+                <Form.Control
+                type="search"
+                placeholder="Type keyword here"
+                className="me-2"
+                aria-label="Type keyword here"
+                ref = {this.titleInput}
+                />
+                <Button variant="outline-success" type="submit" onClick={this.getTitleSearch}>Search</Button>
+            </Form>
+            </div>
+        </div>
+    )
+  }
+}
+
+export default SearchPage
