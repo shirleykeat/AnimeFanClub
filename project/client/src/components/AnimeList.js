dfaeb73ee9c323ab3d60ff9bfb0e70526946a055
Meng Li <dameng@seas.upenn.edu>
2022-08-10 22:11:57 -0400

    Update AnimeList.js

--- project/client/src/components/AnimeList.js
+++ project/client/src/components/AnimeList.js
@@ -1,25 +1,28 @@
 import React from "react";
 import { Card, CardBody } from "shards-react";
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import {
     Row,
     Col
 } from 'antd';
 
-const AnimeList = (props) =>{
+const AnimeList = (props) => {
     return (
         <>
-        {props.animes.map((anime, index)=>(
-        <div className='image-container d-flex justify-content-start m-3'>
-        <Col>
-            <Card style={{ height: '25vh', width: '19vh', overflow: 'auto' }}>
-            <img src={anime.url} referrerpolicy="no-referrer" alt={"animePic"} style={{ height: '25vh' }} />
-            </Card>
-            <NavLink to={{ pathname: '/anime?id=' + anime.id }} style={{ textDecoration: 'none', color: 'brown', fontWeight: 'bold', fontSize: 13 }}>
+            {props.animes.map((anime, index) => (
+                <div className='image-container d-flex justify-content-start m-3'>
+                    <Col>
+                        <Card style={{ height: '25vh', width: '19vh', overflow: 'auto' }}>
+                            <img src={anime.url} referrerpolicy="no-referrer" alt={"animePic"} style={{ height: '25vh' }} />
+                        </Card>
+                        {/*<NavLink to={{ pathname: '/anime?id=' + anime.id }} style={{ textDecoration: 'none', color: 'brown', fontWeight: 'bold', fontSize: 13 }}>
                                 {anime.Name}
-             </NavLink>
-        </Col>
-            </div>
+        </NavLink>*/}
+                        <a href={"../anime?id=" + anime.Anime_id} style={{ textDecoration: 'none', color: 'brown', fontWeight: 'bold', fontSize: 13 }}>
+                            {anime.Name}
+                        </a>
+                    </Col>
+                </div>
             ))}
         </>
     );
