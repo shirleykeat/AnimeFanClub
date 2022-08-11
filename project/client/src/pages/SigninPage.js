70d3cc4156f141a4d6712ba5f7a3699e3c95354d
Meng Li <38992690+shirleykeat@users.noreply.github.com>
2022-08-10 20:02:21 -0400

    updated signin page

--- project/client/src/pages/SigninPage.js
+++ project/client/src/pages/SigninPage.js
@@ -1,6 +1,7 @@
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Alert, Button, Checkbox, Form, Input } from 'antd';
 import React from 'react';
-
+import { useHistory } from 'react-router-dom';
+import { getPassword } from '../fetcher';
 const onFinish = (values) => {
     console.log('Success:', values);
 };
@@ -12,12 +13,60 @@ const onFinishFailed = (errorInfo) => {
 class SigninPage extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            notice: '',
+            username: '',
+            pass: '',
+            truepass: '',
+            id: null,
+            signedin: false
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleUsernameChange = this.handleUsernameChange.bind(this)
+        this.handlePasswordChange = this.handlePasswordChange.bind(this)
+    }
+    handleUsernameChange(event) {
+        this.setState({ username: event.target.value })
+    }
+    handlePasswordChange(event) {
+        this.setState({ pass: event.target.value })
+    }
+    handleSubmit(event) {
+
+        getPassword(this.state.username).then(res => {
+            this.setState({ truepass: res.results[0].Password })
+            this.setState({ id: res.results[0].id })
+        })
+
+        if (this.state.pass !== this.state.truepass) {
+            setTimeout(() => {
+                this.setState({
+                    notice: 'The combination of username and password is incorrect!',
+                });
+            }, 500);
+        }
+        else {
+
+            const history = useHistory();
+            const path = '/user?id=' + this.state.id;
+            history.push(path);
+
+        }
     }
     render() {
         return (
             <div>
                 <div style={{ width: '70vw', margin: '0 auto', marginTop: '10vh' }}>
                     <h1 style={{ color: 'white' }}>sign in</h1></div>
+                {this.state.notice && (
+                    <Alert
+                        style={{ marginBottom: 24 }}
+                        message={this.state.notice}
+                        type="error"
+                        showIcon
+                        closable
+                    />
+                )}
                 <div style={{ width: '70vw', margin: '0 auto', marginTop: '5vh' }}>
                     <Form
                         name="basic"
@@ -44,7 +93,7 @@ class SigninPage extends React.Component {
                                 },
                             ]}
                         >
-                            <Input />
+                            <Input value={this.state.username} onChange={this.handleUsernameChange} />
                         </Form.Item>
 
                         <Form.Item
@@ -57,7 +106,7 @@ class SigninPage extends React.Component {
                                 },
                             ]}
                         >
-                            <Input.Password />
+                            <Input.Password value={this.state.pass} onChange={this.handlePasswordChange} />
                         </Form.Item>
 
                         <Form.Item
@@ -77,7 +126,7 @@ class SigninPage extends React.Component {
                                 span: 16,
                             }}
                         >
-                            <Button type="primary" htmlType="submit">
+                            <Button type="primary" htmlType="submit" onClick={this.handleSubmit}>
                                 Log in
                             </Button>
                         </Form.Item>
